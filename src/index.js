37c8ce7dcc8f1b59498d264f98107723a98cc676
Shlok Motwani <shlok.codes@gmail.com>
2024-08-07 16:50:00 +0530

    insert and delete functionality(all cases)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import { Tree } from "./tree";
 
-let array1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
     11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-let tree1 = new Tree(array1);
-let tree2 = new Tree(array2);
+let tree = new Tree(array);
+console.log("-------------------------------------------------------");
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
 
+tree.insert(50);
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
 
+tree.insert(25);
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
 
-tree1.prettyPrint();
-// tree2.prettyPrint();
+tree.insert(21);
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
 
-tree1.insert(-500);
+tree.insert(26);
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
 
-tree1.prettyPrint();
+//check for left-leaf-node deletion
+tree.deleteItem(17);
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
+
+//check for right-leaf-node deletion
+tree.deleteItem(26);
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
+
+//check for leaf-node-with-left-child deletion
+tree.deleteItem(13);
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
+
+//check for leaf-node-with-right-child deletion
+tree.deleteItem(20);
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
+
+//check for leaf-node-with-both-children deletion
+tree.deleteItem(11);
+tree.prettyPrint();
+console.log("-------------------------------------------------------");
--- src/tree.js
+++ src/tree.js
@@ -7,86 +7,171 @@ class Tree {
   }
 
   buildTree(array) {
-    if(array.length == 1){
-        return new Node(array[0], null, null);
+    if (array.length == 1) {
+      return new Node(array[0], null, null);
     }
     let rootNode;
-    let middleIndex = Math.floor((array.length) / 2);
-    if(middleIndex + 1 >= array.length){
-        rootNode = new Node(array[middleIndex], this.buildTree(array.slice(0, middleIndex)), null);
-    }
-    else{
-        rootNode = new Node(array[middleIndex], this.buildTree(array.slice(0, middleIndex)), this.buildTree(array.slice(middleIndex+1, array.length)));
+    let middleIndex = Math.floor(array.length / 2);
+    if (middleIndex + 1 >= array.length) {
+      rootNode = new Node(
+        array[middleIndex],
+        this.buildTree(array.slice(0, middleIndex)),
+        null
+      );
+    } else {
+      rootNode = new Node(
+        array[middleIndex],
+        this.buildTree(array.slice(0, middleIndex)),
+        this.buildTree(array.slice(middleIndex + 1, array.length))
+      );
     }
     return rootNode;
   }
 
   linearSortWithoutDuplicates(array) {
     for (let i = 0; i < array.length; i++) {
-        let indexOfSmallest = i;
-        for(let j=i+1; j<array.length; j++){
-            if(array[j] < array[indexOfSmallest]){
-                indexOfSmallest = j;
-            }
+      let indexOfSmallest = i;
+      for (let j = i + 1; j < array.length; j++) {
+        if (array[j] < array[indexOfSmallest]) {
+          indexOfSmallest = j;
         }
-        let temp = array[i];
-        array[i] = array[indexOfSmallest];
-        array[indexOfSmallest] = temp;
+      }
+      let temp = array[i];
+      array[i] = array[indexOfSmallest];
+      array[indexOfSmallest] = temp;
     }
     let newArray = [];
-    for(let i=0; i<array.length; i++){
-        if(array[i] != array[i+1]){
-            newArray.push(array[i]);
-        }
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] != array[i + 1]) {
+        newArray.push(array[i]);
+      }
     }
     console.log(newArray);
     return newArray;
   }
 
-  prettyPrint(node = this.root, prefix = "", isLeft = true){
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
     if (node === null) {
       return;
     }
     if (node.right !== null) {
-      this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
     }
     console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
     if (node.left !== null) {
-        this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
-  };
+  }
 
-  insert(value){
-    if(!value){
-        console.log("Enter a valid value");
-        return;
+  insert(value) {
+    console.log(`insertValue ${value} called.`)
+    if (!value) {
+      console.log("Enter a valid value");
+      return;
     }
     let temp = this.root;
     let newNode = new Node(value, null, null);
     console.log("value = " + value);
     console.log("temp node = " + temp.data);
-    while(true){
-        if(value == temp.data){
-            console.log('Value already exists in the tree!');
-            return;
+    while (true) {
+      if (value == temp.data) {
+        console.log("Value already exists in the tree!");
+        return;
+      }
+      if (value < temp.data) {
+        if (!temp.left) {
+          temp.left = newNode;
+          return;
         }
-        if(value < temp.data){
-            if(!temp.left){
-                temp.left = newNode;
-                return;
-            }
-            temp = temp.left;
+        temp = temp.left;
+      } else {
+        if (!temp.right) {
+          temp.right = newNode;
+          return;
         }
-        else{
-            if(!temp.right){
-                temp.right = newNode;
-                return;
-            }
-            temp = temp.right;
-        }
+        temp = temp.right;
+      }
     }
   }
- 
+
+  deleteItem(value) {
+    console.log(`deleteItem ${value} called.`);
+    let temp = this.root;
+    let prev;
+    let isNodeALeftChild = undefined;
+
+    //search for target node
+    while (true) {
+      //if temp reaches null, TARGET NODE NOT FOUND in the tree
+      if (!temp) {
+        console.log("Item does not exist in the tree.");
+        return;
+      }
+      //temp traverses the tree to reach target node
+      if (temp.data > value) {
+        //move temp towards the left
+        prev = temp;
+        temp = temp.left;
+        isNodeALeftChild = true;
+      } else if (temp.data < value) {
+        //move temp towards the right
+        prev = temp;
+        temp = temp.right;
+        isNodeALeftChild = false;
+      } else {
+        // TARGET NODE FOUND
+        // if target node is a leaf node
+        if (!temp.left && !temp.right) {
+          if (prev.left == temp) {
+            prev.left = null;
+          } else {
+            prev.right = null;
+          }
+          return;
+        }
+        else{
+            //if target node has ONLY a left child
+            if(!temp.right){
+              if(isNodeALeftChild){
+                prev.left = temp.left;
+              }
+              else{
+                prev.right = temp.left;
+              }
+              temp.left = null;
+            }
+            //if target node has ONLY a right child
+            else if(!temp.left){
+              if(isNodeALeftChild){
+                prev.left = temp.right;
+              }
+              else{
+                prev.right = temp.right;
+              }
+              temp.right = null;
+            }
+            //if target node has BOTH a left AND a right child
+            else{
+              let targetNode = temp;
+              prev = temp;
+              temp = temp.right;
+              while(temp.left){
+                prev = temp;
+                temp = temp.left;
+              }
+              prev.left = temp.right;
+              temp.right = null;
+              targetNode.data = temp.data;
+            }
+        }
+        return;
+      }
+    }
+  }
 }
 
 export { Tree };
